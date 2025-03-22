6b24afc37620c30a58d2ee9df908d2f9660b41df
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-22 18:18:51 +0530

    FIX:complete not working code

--- QUEENS_EXTENSION/queens.js
+++ QUEENS_EXTENSION/queens.js
@@ -7,7 +7,7 @@ document.getElementById("pop").addEventListener("click", async () => {
     });
 });
 function solveIt(){
-    let iframe_coll=document.getElementsByClassName("game-launch-page__iframe")[0];
+    let iframe_coll=document.getElementsByClassName("game-launch-page__iframe w-full")[0];
     //console.log(iframe_coll);
     let iframe=iframe_coll.contentDocument||iframe_coll.contentWindow?.document;
     const content=iframe.getElementsByClassName("queens-cell-with-border");
@@ -28,11 +28,13 @@ function solveIt(){
     let col=Array(size).fill(false);
     let clr=new Map();
     const locked=[]
+    
     Array.from(board).forEach((val,idx)=>{
         if(val==1){
             row[Math.floor(idx/size)]=true;
             col[idx%size]=true;
             locked.push(idx);
+            clr.set(clr.has(val.classList[1].slice(-1)),true);
         }
     });
     //console.log(row,col);
@@ -52,7 +54,7 @@ function solveIt(){
         let r_idx=Math.floor(idx/size);
         let c_idx=idx%size;
 
-        let directions=[[0,1],[1,1],[1,0],[1,1],[0,-1],[-1,0],[-1,-1],[0,0]];
+        let directions=[[0,1],[1,0],[1,1],[0,-1],[-1,0],[-1,-1],[1,-1],[-1,1]];
 
         return directions.some(([dx,dy])=>{
             let nx=r_idx+dx;
@@ -73,37 +75,62 @@ function solveIt(){
             console.log(board.slice(i,i+size));
         }
     }
-    //display(board);
-    function backtrack(idx){
-        if(idx===board.length){
-            console.log("FOUND!");
-            display(board);
-            console.log(row,col,clr);
+    display(board);
+
+    console.log("CHECKPOINT-1");
+    console.log(row,col,clr);
+    display(board);
+
+    function backtrack(r){
+        if(r===board.length){
             return true;
         }
-
-        if(board[idx]===1 || locked.includes(idx)){
-            return backtrack(idx+1);
+        for(let c=0;c<size;c++){
+            let nidx=r*size+c;
+            let clrr=grid_clr.get(nidx);
+            if(is_safe(nidx,r,c,clrr)){
+                board[nidx]=1;
+                row[r]=true;
+                col[c]=true;
+                clr.set(clrr, true);
+                if(backtrack(r+1))return true;
+                board[nidx]=0;
+                row[r]=false;
+                col[c]=false;
+                clr.set(clrr,false);
+            }
         }
-        let r=Math.floor(idx/size);
-        let c=idx%size;
-        let clrr=grid_clr.get(idx);
-        if(is_safe(idx,r,c,clrr)){
-            board[idx]=1
-            row[r]=true;
-            col[c]=true;
-            clr.set(clrr,true);
-
-            if(backtrack(idx+1))return true;
-            
-            board[idx]=0
-            row[r]=false;
-            col[c]=false;
-            clr.set(clrr,false);
-        }
-        return backtrack(idx+1);
+        return false;
     }
     backtrack(0);
-    //display(board);
-    //console.log(board);
+    function placeQueenAtIndex(index){
+        let tile = iframe.querySelector(`[data-cell-idx="${index}"]`);
+    
+        if (tile) {
+            console.log(`Clicking tile with index ${index}`);
+    
+            function clickElement(element) {
+                let down = new MouseEvent("mousedown", { bubbles: true, cancelable: true });
+                let up = new MouseEvent("mouseup", { bubbles: true, cancelable: true });
+                let click = new MouseEvent("click", { bubbles: true, cancelable: true });
+    
+                element.dispatchEvent(down);
+                element.dispatchEvent(up);
+                element.dispatchEvent(click);
+            }
+    
+            clickElement(tile);
+            setTimeout(() => clickElement(tile), 100);
+        } else {
+            console.log("Tile not found!");
+        }
+    }
+    let populate=(()=>{
+        for(let i=0;i<board.length;i++){
+            if(board[i]===1 && !locked.includes(i)){
+                placeQueenAtIndex(i);
+            }
+        }
+    });
+    populate();
 }
