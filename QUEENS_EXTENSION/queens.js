152d50b6c7cc46a7f7138e89409c44c2d6b7d5e7
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-22 15:30:27 +0530

    FEAT:getting bool row,col,fetching rows

--- QUEENS_EXTENSION/queens.js
+++ QUEENS_EXTENSION/queens.js
@@ -13,14 +13,37 @@ function solveIt(){
     const content=iframe.getElementsByClassName("queens-cell-with-border");
     //console.log(content);
     let board=Array.from(content,(el)=>el.getAttribute("aria-label").includes("Queen")?1:0);
-    console.log(board);
+    //console.log(board);
 
     let size=Math.sqrt(board.length);
-    console.log(size);
+    //console.log(size);
 
     let grid_clr=new Map();
-    let add_to_map=(idx,value)=> !grid_clr.has(value)?grid_clr.set(value,[idx]):grid_clr.get(value).push(idx);
-    Array.from(content).forEach((val,idx)=>add_to_map(idx,val.classList[1].slice(-1)));
+    //let add_to_map=(idx,value)=> !grid_clr.has(value)?grid_clr.set(value,[idx]):grid_clr.get(value).push(idx);
+    Array.from(content).forEach((val,idx)=>grid_clr.set(idx,val.classList[1].slice(-1)));
 
     console.log([...grid_clr]);
+
+    let get_row=(idx)=>{
+        let row_start_index=Math.floor(idx/size)*size;
+        return Array.from({length:size},(_,i)=>board[row_start_index+i]);
+    };
+
+    let get_col=(idx)=>{
+        let col_start_index=idx%size;
+        return Array.from({length:size},(_,i)=>board[col_start_index+i*size]);
+    };
+    let row=Array(size).fill(false);
+    let col=Array(size).fill(false);
+    let clr=new Map();
+    
+
+    Array.from(board).forEach((val,idx)=>{
+        if(val==1){
+            row[Math.floor(idx/size)]=true;
+            col[idx%size]=true;
+        }
+    });
+
+    
 }
